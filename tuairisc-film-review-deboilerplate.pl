43513eee54dd491425a3d2a37ba374488862e780
Jim O'Regan <joregan@gmail.com>
2017-03-21 21:14:46 +0000

    some articles have no <span>

--- tuairisc-film-review-deboilerplate.pl
+++ tuairisc-film-review-deboilerplate.pl
@@ -23,6 +23,9 @@ while(<LIST>) {
 	my $tree = HTML::TreeBuilder::XPath->new_from_url($url);
 
 	my $nodes = $tree->findnodes('//div[@class="article--full__content"]/p/span');
+	if($nodes->size() == 0) {
+		$nodes = $tree->findnodes('//div[@class="article--full__content"]/p');
+	}
 	for my $n ($nodes->string_values()) {
 		print OUT "$n\n";
 	}
